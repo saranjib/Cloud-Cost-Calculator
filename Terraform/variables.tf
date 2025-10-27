ee2ccb9a536e78e504fc8bc40b980c5485e8fcf5
saranjib <102132535+saranjib@users.noreply.github.com>
2025-10-27 10:48:21 +0530

    Add storage_account_name variable to variables.tf
    Added variable for storage account name with default value.

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -3,3 +3,9 @@ variable "resource_group_location" {
   default     = "eastus"
   description = "Location of the resource group."
 }
+
+variable "storage_account_name" {
+  type        = string
+  default     = "costsa12345"
+  description = "Name of the storage account for cost exports and reports."
+}
